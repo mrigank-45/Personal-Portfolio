cee797da89710401e23f666091851f59ea87e92a
Mrigank <108195614+mrigank-45@users.noreply.github.com>
2023-10-11 22:55:51 +0530

    changed about me

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,9 @@
 setTimeout(function () {
   $('.about-info-inner').typed({
     strings: [
-      "<span>Hi, I am Mrigank Shukla.</span> I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications. I am also interested in Blockchain development using Solidity, and can create <span>blockchain-based applications</span>. I have a good hold over developing Decentralized Applications or DApps using solidity smart-contracts. I have also done <span>internships as a Full Stack Developer</span> (recently at Andwemet Services pvt ltd). I have worked on several large scale projects and created beautiful and functional websites. More information about my projects is available at GitHub | Mrigank Shukla. Apart from coding, I love to read technical blogs on the latest and emerging technologies. I'm always ready to have new experiences, meet new people and work in the latest fast paced environments."
+      "<span>Hi, I am Mrigank Shukla.</span> I am a passionate Full Stack Developer who loves to explore new technologies and build scalable applications. I am also interested in Blockchain development can create <span>blockchain-based applications</span>. I have a good hold over developing Decentralized Applications or DApps using solidity smart-contracts. I have also done multiple internships both as a blockchain developor and Full Stack Web Developer. I have also participated in multiple all india level hackathons and won prices. I have worked on several large scale projects and created beautiful and functional websites. More information about my projects is available at GitHub | Mrigank Shukla. Apart from coding, I love to read technical blogs on the latest and emerging technologies. I'm always ready to have new experiences, meet new people and work in the latest fast paced environments."
     ],
-    typeSpeed: 25,
+    typeSpeed: 50,
     contentType: 'html'
   });
 }, 500);
